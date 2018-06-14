b3ba1e1d53808f8bde475a54a643fec0d1066c86
Nicholas Finch <ngf4@nau.edu>
2018-06-14 10:12:24 -0700

    cutting down on repetitive code

--- get_data.php
+++ get_data.php
@@ -14,22 +14,6 @@ $sql = "
 
 mysqli_query($cxn, $sql);
 
-$sql = "
-        INSERT INTO `computers` (`ComputerName`, `usage_0`, `usage_10`, `usage_20`, `usage_30`, `usage_40`, `usage_50`, `usage_60`, `usage_70`, `usage_80`, `usage_90`, `users`, `OrderDate`)
-        VALUES (
-        '" . mysqli_real_escape_string($cxn, $_POST['ComputerName']) . "',
-        '" . mysqli_real_escape_string($cxn, $_POST['usage_0']) . "',
-        '" . mysqli_real_escape_string($cxn, $_POST['usage_10']) . "',
-        '" . mysqli_real_escape_string($cxn, $_POST['usage_20']) . "',
-        '" . mysqli_real_escape_string($cxn, $_POST['usage_30']) . "',
-        '" . mysqli_real_escape_string($cxn, $_POST['usage_40']) . "',
-        '" . mysqli_real_escape_string($cxn, $_POST['usage_50']) . "',
-        '" . mysqli_real_escape_string($cxn, $_POST['usage_60']) . "',
-        '" . mysqli_real_escape_string($cxn, $_POST['usage_70']) . "',
-        '" . mysqli_real_escape_string($cxn, $_POST['usage_80']) . "',
-        '" . mysqli_real_escape_string($cxn, $_POST['usage_90']) . "',
-        '" . mysqli_real_escape_string($cxn, $_POST['users']) . "',
-        '" . mysqli_real_escape_string($cxn, $_POST['OrderDate']) . "');
-";
-
+$fields = ['ComputerName', 'usage_0', 'usage_10', 'usage_20', 'usage_30', 'usage_40', 'usage_50', 'usage_60', 'usage_70', 'usage_80', 'usage_90', 'users', 'OrderDate'];
+$sql = composeInsertSql($fields, getValuesFromPost($fields));
 mysqli_query($cxn, $sql);
