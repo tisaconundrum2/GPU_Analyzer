fb1cdaa07c77b5fe4b83a9e12489e472229905dd
Nicholas Finch <ngf4@nau.edu>
2018-06-15 15:04:09 -0700

    Cleaning up the repetitive echo

--- index.php
+++ index.php
@@ -75,21 +75,23 @@ $id = $_GET['id'];
                     $result = mysqli_query($cxn, "SELECT DISTINCT ComputerName FROM computers");
                     while ($row = mysqli_fetch_array($result)) {
                         if ($row['ComputerName'] != null) {
-                            echo "<li class=\"nav-item\">";
-                            echo "<a class=\"nav-link\" href=\".?id=" . $row['ComputerName'] . "\">";
-                            echo "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\"";
-                            echo "fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\"";
-                            echo "stroke-linejoin=\"round\" class=\"feather feather-layers\">";
-                            echo "<polygon points=\"12 2 2 7 12 12 22 7 12 2\"></polygon>";
-                            echo "<polyline points=\"2 17 12 22 22 17\"></polyline>";
-                            echo "<polyline points=\"2 12 12 17 22 12\"></polyline>";
-                            echo "</svg>";
-                            echo $row['ComputerName'];
-                            echo "</a>";
-                            echo "</li>";
+                            printf("
+                                    <li class=\"nav-item\">
+                                        <a class=\"nav-link\" href=\".?id=%s\">
+                                            <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"feather feather-layers\">
+                                            <polygon points=\"12 2 2 7 12 12 22 7 12 2\"></polygon>
+                                            <polyline points=\"2 17 12 22 22 17\"></polyline>
+                                            <polyline points=\"2 12 12 17 22 12\"></polyline>
+                                            </svg>
+                                        %s
+                                        </a>
+                                    </li>
+                            ", $row['ComputerName'], $row['ComputerName']);
                         }
                     }
                     ?>
+
+
                 </ul>
             </div>
         </nav>
