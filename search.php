7aabeb1bccf707b06b02b94afdaa9d8f414db78c
Nicholas Finch <ngf4@nau.edu>
2018-06-15 14:02:39 -0700

    make it a link

--- search.php
+++ search.php
@@ -135,7 +135,7 @@ $q = $_GET['q'];
                     $result = mysqli_query($cxn, "SELECT DISTINCT ComputerName FROM computers WHERE ComputerName LIKE '%$q%'");
                     while ($row = mysqli_fetch_array($result)) {
                         echo "<tr>";
-                        echo "<td>" . $row['ComputerName'] . "</td>";
+                        echo "<td><a href=\".?id=" . $row['ComputerName'] . "\"></a></td>";
                         echo "</tr>";
                     }
                     ?>
