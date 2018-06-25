b91b0e0da2c2a14e6e3bcdb2972b09de98b38765
Nicholas Finch <ngf4@nau.edu>
2018-06-25 12:44:55 -0700

    Confirming that the tests work

--- php/test.php
+++ php/test.php
@@ -1,12 +1,18 @@
 <?php
-include __DIR__ . '/../src/connect.php';
+include __DIR__ . '/../../src/connect.php';
 if (mysqli_connect_errno()) {
     echo "Connect failed: ";
     echo $mysqli_connect_error();
 }
 
-$query = "SELECT DISTINCT users FROM computers WHERE ComputerName='EN4073254W'";
-$stmtQuery = mysqli_query($cxn, $query);
-while ($user_name = mysqli_fetch_array($stmtQuery)) {
-    printf("<li><a href='%s'>%s</a>\n", $user_name[0], $user_name[0]);
+$stmtQuery1 = mysqli_query($cxn, "SELECT DISTINCT ComputerName FROM computers");
+while ($comp_name = mysqli_fetch_array($stmtQuery1)) {
+    printf("%s\n", $comp_name[0]);
+
+    $stmtQuery2 = mysqli_query($cxn, "SELECT DISTINCT users FROM computers WHERE ComputerName='$comp_name[0]'");
+    while ($user_name = mysqli_fetch_array($stmtQuery2)) {
+        printf("\t%s\n",$user_name[0]);
+    }
+    printf("\n");
+
 }
