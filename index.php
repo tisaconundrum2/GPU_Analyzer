4c93b30db819452a6e3fd8c564061a3fc9500624
Nicholas Finch <ngf4@nau.edu>
2018-07-10 15:11:52 -0700

    instead of using a bunch of if statements. Use string concatenation

--- index.php
+++ index.php
@@ -97,17 +97,14 @@ $question = $_GET['q'];
                         <button class="btn btn-sm btn-outline-secondary">Share</button>
                         <button class="btn btn-sm btn-outline-secondary">Export</button>
                     </div>
-                    <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                             class="feather feather-calendar">
-                            <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
-                            <line x1="16" y1="2" x2="16" y2="6"></line>
-                            <line x1="8" y1="2" x2="8" y2="6"></line>
-                            <line x1="3" y1="10" x2="21" y2="10"></line>
-                        </svg>
-                        This week
-                    </button>
+                    <form action="" style="margin-bottom: 0px;">
+                        <div class="btn-group mr-2">
+                            <input type="date" name="bday" class="form-control">
+                            <button type="submit" class="btn btn-sm btn-outline-secondary">Submit</button>
+
+                        </div>
+                    </form>
+
                 </div>
             </div>
 
@@ -143,7 +140,8 @@ $question = $_GET['q'];
                     </thead>
                     <tbody>
                     <?php
-                    $comp_result = queryComputerUsages($cxn, $user, $id);
+
+                    $comp_result = queryComputerUsages($cxn, $user, $date, $id);
                     while ($user_name = mysqli_fetch_array($comp_result)) {
                         echo "<tr>";
                         table_td($user_name['ComputerName']);
--- php/page_functions.php
+++ php/page_functions.php
@@ -51,12 +51,18 @@ function table_td($string)
     echo "<td>" . $string . "</td>";
 }
 
-function queryComputerUsages($cxn, $user, $id, $date, $days=5)
+function queryComputerUsages($cxn, $user, $id, $date, $days = 5)
 {
-    $p_date = strtotime("+$days day", strtotime($date));
-    if ($user != null) {
-        return mysqli_query($cxn, "SELECT * FROM computers WHERE ComputerName='$id' AND users='$user' AND OrderDate BETWEEN '$date' AND '$p_date' ORDER BY `computers`.`OrderDate` ASC ");
-    } else {
-        return mysqli_query($cxn, "SELECT * FROM computers WHERE ComputerName='$id' AND OrderDate BETWEEN '$date' AND '$p_date' ORDER BY `computers`.`OrderDate` ASC ");
+    echo stop;
+    $p_date = date("Y-m-d", strtotime("+$days day", strtotime($date)));
+    $full_query = "SELECT * FROM computers WHERE ComputerName='$id' ";
+    if ($user) {
+        $full_query .= "AND users='$user' ";
     }
+
+    if ($date) {
+        $full_query .= "AND OrderDate BETWEEN '$date' AND '$p_date' ";
+    }
+    $full_query .= "ORDER BY `computers`.`OrderDate` ASC";
+    return $full_query;
 }
