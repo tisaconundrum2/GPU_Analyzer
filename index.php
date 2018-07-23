019548ca270747f454f8948e85cd972610b779b7
Nicholas Finch <ngf4@nau.edu>
2018-07-23 12:05:32 -0700

    Organizing

--- index.php
+++ index.php
@@ -10,71 +10,17 @@ if (mysqli_connect_errno()) {
 $id = $_GET['id'];
 $user = $_GET['user'];
 $date = $_GET['date'];
+$question = $_GET['q'];
+runHead();
+echo "<body>";
+runTopSticky();
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description"
-          content="GPUtilization. A website dedicated to helping IT gather information on GPU utilizations across the network">
-    <meta name="author" content="Nicholas Finch">
-    <link rel="icon" href="https://getbootstrap.com/favicon.ico">
 
-    <title>Dashboard for GPUtil</title>
-
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <!-- Custom styles for this template -->
-    <link href="css/dashboard.css" rel="stylesheet">
-    <style type="text/css">/* Chart.js */
-        @-webkit-keyframes chartjs-render-animation {
-            from {
-                opacity: 0.99
-            }
-            to {
-                opacity: 1
-            }
-        }
-
-        @keyframes chartjs-render-animation {
-            from {
-                opacity: 0.99
-            }
-            to {
-                opacity: 1
-            }
-        }
-
-        .chartjs-render-monitor {
-            -webkit-animation: chartjs-render-animation 0.001s;
-            animation: chartjs-render-animation 0.001s;
-        }</style>
-</head>
-
-<body>
-<form action="">
-    <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="index.php">ASU - GPU utilizations</a>
-        <input class="form-control form-control-dark w-100" placeholder="Search" aria-label="Search" type="text" name="q">
-        <!--    Sign in button. Not applicable quite yet    -->
-        <!--        <ul class="navbar-nav px-3">-->
-        <!--            <li class="nav-item text-nowrap">-->
-        <!--                <a class="nav-link" href="#">Sign out</a>-->
-        <!--            </li>-->
-        <!--        </ul>-->
-    </nav>
-</form>
 
 <div class="container-fluid">
     <div class="row">
-        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-            <div class="sidebar-sticky">
-                <ul class="nav flex-column" id="sidebar">
-                </ul>
-            </div>
-        </nav>
+        <?php runSideSticky();?>
 
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
             <div style="position: absolute; left: 0; top: 0; right: 0; bottom: 0; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;"
@@ -101,7 +47,6 @@ $date = $_GET['date'];
                             <button type="submit" class="btn btn-sm btn-outline-secondary">Submit</button>
                         </div>
                     </form>
-
                 </div>
             </div>
 
--- php/page_functions.php
+++ php/page_functions.php
@@ -1,7 +1,6 @@
 <?php
 
 
-
 function table_td($string)
 {
     echo "<td>" . $string . "</td>";
@@ -20,4 +19,81 @@ function queryComputerUsages($cxn, $user, $id, $date, $days = 5)
     }
     $full_query .= "ORDER BY `computers`.`OrderDate` ASC";
     return mysqli_query($cxn, $full_query);
+}
+
+function runHead()
+{
+    printf("
+<!DOCTYPE html>
+<html lang=\"en\">
+<head>
+    <meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\">
+    <meta charset=\"utf-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
+    <meta name=\"description\"
+          content=\"GPUtilization. A website dedicated to helping IT gather information on GPU utilizations across the network\">
+    <meta name=\"author\" content=\"Nicholas Finch\">
+    <link rel=\"icon\" href=\"https://getbootstrap.com/favicon.ico\">
+
+    <title>Dashboard for GPUtil</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel=\"stylesheet\" href=\"css/bootstrap.css\">
+    <!-- Custom styles for this template -->
+    <link href=\"css/dashboard.css\" rel=\"stylesheet\">
+    <style type=\"text/css\">/* Chart.js */
+        @-webkit-keyframes chartjs-render-animation {
+            from {
+                opacity: 0.99
+            }
+            to {
+                opacity: 1
+            }
+        }
+
+        @keyframes chartjs-render-animation {
+            from {
+                opacity: 0.99
+            }
+            to {
+                opacity: 1
+            }
+        }
+
+        .chartjs-render-monitor {
+            -webkit-animation: chartjs-render-animation 0.001s;
+            animation: chartjs-render-animation 0.001s;
+        }</style>
+</head>    
+");
+}
+
+function runTopSticky()
+{
+    printf("
+<form action=\"\">
+    <nav class=\"navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0\">
+        <a class=\"navbar-brand col-sm-3 col-md-2 mr-0\" href=\"index.php\">ASU - GPU utilizations</a>
+        <input class=\"form-control form-control-dark w-100\" placeholder=\"Search\" aria-label=\"Search\" type=\"text\" name=\"q\">
+        <!--    Sign in button. Not applicable quite yet    -->
+        <!--        <ul class=\"navbar-nav px-3\">-->
+        <!--            <li class=\"nav-item text-nowrap\">-->
+        <!--                <a class=\"nav-link\" href=\"#\">Sign out</a>-->
+        <!--            </li>-->
+        <!--        </ul>-->
+    </nav>
+</form>
+");
+}
+
+function runSideSticky()
+{
+    printf("
+<nav class=\"col-md-2 d-none d-md-block bg-light sidebar\">
+    <div class=\"sidebar-sticky\">
+        <ul class=\"nav flex-column\" id=\"sidebar\">
+        </ul>
+    </div>
+</nav>    
+");
 }
