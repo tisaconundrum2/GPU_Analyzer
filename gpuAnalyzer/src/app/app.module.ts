cdb47c806b63c7159b01cc97c0f35fa12ab26a5e
Nicholas Finch <ngf4@nau.edu>
2018-08-08 13:57:44 -0700

    Adding initial pieces to the UI

--- gpuAnalyzer/src/app/app.module.ts
+++ gpuAnalyzer/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    DashboardComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
