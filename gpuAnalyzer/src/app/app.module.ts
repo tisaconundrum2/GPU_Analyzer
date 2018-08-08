e21a47c773895b3d0de9050aa79a57e755cd6a28
Nicholas Finch <ngf4@nau.edu>
2018-08-08 14:37:35 -0700

    Fixing formatting, adding styles and scripts

--- gpuAnalyzer/src/app/app.module.ts
+++ gpuAnalyzer/src/app/app.module.ts
@@ -1,22 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import {AppComponent} from './app.component';
+import {SidebarComponent} from './sidebar/sidebar.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {NavbarComponent} from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SidebarComponent,
-    DashboardComponent,
-    NavbarComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        SidebarComponent,
+        DashboardComponent,
+        NavbarComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
