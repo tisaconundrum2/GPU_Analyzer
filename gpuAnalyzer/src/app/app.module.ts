bcba300a762407574082a5168b8075e0c51088c0
Nicholas Finch <ngf4@nau.edu>
2018-08-09 11:37:39 -0700

    Added all necessary pieces to get started

--- gpuAnalyzer/src/app/app.module.ts
+++ gpuAnalyzer/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
